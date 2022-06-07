e4532b369790cdab903ddbe509c4bd895e2058dc
krishna murthy <dhulipallakrishna2367@gmail.com>
2022-06-07 16:04:31 +0530

    initial commit

---
+++ demo.js
@@ -0,0 +1,8 @@
+type="text/javascript"
+function imgslider(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+function changecolor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background=color;
+}
